b5b02a6cb5b71543b49bb89aee09a7a71cce899c
RobertMJr <25443406+RobertMJr@users.noreply.github.com>
2020-08-16 22:46:20 +0300

    Added functions to get and display users

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,37 @@
 const peopleUrl = 'https://randomuser.me/api/?results=12';
+const galleryDiv = document.getElementById('gallery');
 
 async function getData(url) {
     const userResponse = await fetch(url);
     const userJSON = await userResponse.json();
 
-    return console.log(userJSON.results);
+    const users = userJSON.results.map(async user => user);
+    return Promise.all(users);
 }
 
-getData(peopleUrl);
+
+
+function generateGallery(data) {
+    data.map(person => {
+        const divEl = document.createElement('div');
+        galleryDiv.appendChild(divEl);
+        divEl.className = 'card';
+        divEl.innerHTML = `
+        <div class="card-img-container">
+            <img class="card-img" src="${person.picture.large}" alt="profile picture">
+        </div>
+        <div class="card-info-container">
+            <h3 id="${person.name.last + person.name.first}" class="card-name cap">${person.name.first} ${person.name.last}</h3>
+            <p class="card-text">${person.email}</p>
+            <p class="card-text cap">${person.location.city}, ${person.location.state}</p>
+        </div>
+        `;
+
+    });
+}
+
+async function execute () {
+    const userProfiles = await getData(peopleUrl);
+    generateGallery(userProfiles);    
+}
+execute();
